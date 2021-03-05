8618494d51dd64082ba795293c54350f74dbd8bb
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-05 10:26:33 -0800

    add constructor and test getting object property

--- Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+
 using Tracker.Models;
 using System;
 
@@ -11,8 +12,26 @@ namespace Tracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesObjectOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("test", "test", 1, "1");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_Title()
+    {
+      //Arrange
+      string title = "Pastry";
+      string description = "";
+      int price = 3;
+      string date = "12/02/2000";
+
+      //Act
+      Order newOrder = new Order(title, description, price, date);
+      string result = newOrder.Title;
+
+      //Assert
+      Assert.AreEqual(title, result);
+    }
+
   }
 }
--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -1,12 +1,22 @@
 using System.Collections.Generic;
 
+
 namespace Tracker.Models
 {
   public class Order
   {
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Price { get; set; }
+    public int Price { get; set; }
     public string Date { get; set; }
+  
+
+    public Order(string title, string description, int price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+    }
   }
 }
