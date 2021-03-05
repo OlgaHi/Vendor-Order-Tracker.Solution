b463fa1f7122b3d20c793b4638b2dff660f3d7f1
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-05 10:47:30 -0800

    add static List and Id properties

--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -9,6 +9,8 @@ namespace Tracker.Models
     public string Description { get; set; }
     public int Price { get; set; }
     public string Date { get; set; }
+    public int Id { get; } 
+    private static List<Order> _instances = new List<Order> { };
   
 
     public Order(string title, string description, int price, string date)
@@ -17,6 +19,8 @@ namespace Tracker.Models
       Description = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
   }
 }
