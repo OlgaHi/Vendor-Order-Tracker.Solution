8dc1e90c0b7500f065a0ec0f3f512f6f9f4eab0e
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-05 11:08:07 -0800

    add vendor class with properties and test instance creation

---
+++ Tracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Tracker.Tests
+{
+  [TestClass]
+  public class VendorTests 
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
--- Tracker/Models/Vendor.cs
+++ Tracker/Models/Vendor.cs
@@ -1,9 +1,13 @@
-// using System.Collections.Generic
+using System.Collections.Generic;
 
-// namespace Tracker.Models
-// {
-//   public class Vendor
-//   {
+namespace Tracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    
 
-//   }
-// }
+  }
+}
